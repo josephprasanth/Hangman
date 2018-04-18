4a3b480649be64dfb499827cdd4e23b2c09212a8
josephprasanth <prasanthjoseph_77@yahoo.com>
2018-04-18 17:00:04 +0200

    Creating basic input parameters and random number function

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-var app = angular.module
+var app = angular.module('HangmanApp',[]);
+app.controller('GameController', ['$scope', function ($scope) {
+   var words = ['rat','cat','bat','mat','success','infinity'];
+   $scope.incorrectLettersChoosen = [];
+   $scope.correctLettersChoosen = [];
+   $scope.guesses = 6;
+   $scope.displayWords = '';
+   $scope.input = {
+      letter: ''
+    }
+     
+    var selectRandomWord = function () {
+        var index = Math.round(Math.random()* words.length);
+        console.log(index);
+
+    }
+
+
+}]);
